7911ff916339c42243d22ef69b1f97f21c59599e
birdalicous <jack.bird@durham.ac.uk>
2021-07-13 00:16:53 +0100

    added picom config

--- scripts/linkfiles.sh
+++ scripts/linkfiles.sh
@@ -11,3 +11,7 @@ mkdir ~/.config/nvim
 rm ~/.config/nvim/init.vim
 ln ./nvim/init.vim ~/.config/nvim
 echo "nvim config linked"
+
+sudo rm /etc/xdg/picom.conf
+sudo ln -s picom.conf /etc/xdg/
+echo "picom config linked"
