268db6995d7f4f401edb38d4e98f6c59a542a9a0
Jack Bird <jack.bird@durham.ac.uk>
2021-10-19 11:31:10 +0100

    Added basic installs

--- scripts/installs.sh
+++ scripts/installs.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 #Install packages
+sudo pacman -S --noconfirm xorg-server xorg-xinit xorg-xbacklight picom
+sudo pacman -S --noconfirm base-devel make
 sudo pacman -S --nocomfirm git
+sudo pacman -S --nocomfirm openssh
 sudo pacman -S --noconfirm neovim
 sudo pacman -S --noconfirm fzf
 sudo pacman -S --noconfirm firefox
 sudo pacman -S --noconfirm redshift
 sudo pacman -S --noconfirm keepassxc
 sudo pacman -S --noconfirm rofi
+sudo pacman -S --noconfirm tlp acpi_call
 sudo pacman -S --noconfirm feh
+
